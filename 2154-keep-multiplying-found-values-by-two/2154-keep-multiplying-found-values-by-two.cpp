0dfeb9a36b77dd2335bd99641b74c2c98fd25993
JYOTI ANJALY <68936339+jyoti-anjaly@users.noreply.github.com>
2022-01-31 21:47:56 +0530

    Time: 12 ms (20.00%), Space: 11.6 MB (10.00%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -20,7 +20,7 @@ public:
         {
             int mid=(left +right)/2;
             if(arr[mid]==target) return mid;
-            else if(arr[mid]>target) right=mid;
+            else if(arr[mid]>target) right=mid-1;
             else left=mid+1;
         }
         return left;
