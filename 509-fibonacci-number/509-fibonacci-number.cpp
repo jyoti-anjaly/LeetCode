5eabbff58901a624cd4746fb82b290ff23cb7349
JYOTI ANJALY <68936339+jyoti-anjaly@users.noreply.github.com>
2022-07-06 13:18:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<2)return n;
+        int prev2=0,prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;            
+        }
+        return prev1;
+    }
+};
